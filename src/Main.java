112437f5bf29116657f788e9ed48ac2f0773e868
ibmsc <Ibm.sc1409@gmail.com>
2022-09-18 11:59:18 +0300

    1 commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
-public class Main {
-    public static void main(String[] args) {
+import java.util.*;
 
-    }
+public class Main {
+
+    private static final List<Integer> nums = new ArrayList<>(List.of(1, 1, 2, 3, 4, 4, 5, 5, 6, 7));
+    private static final List<String> words = new ArrayList<>(List.of("abc", "abc", "cde", "cde", "e","cde","f"));
+
 }
